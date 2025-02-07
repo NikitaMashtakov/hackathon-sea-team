f2cc109b1af89e069ae5f302dbb658d8772b7a28
Nikita Mashtakov <n-mashtakov@yandex.ru>
2025-02-07 17:53:24 +0700

    add menu opener

--- src/app.js
+++ src/app.js
@@ -1,2 +1,12 @@
-import './styles.css'
+import { ContextMenu } from "./menu";
+import "./styles.css";
 
+document.addEventListener("DOMContentLoaded", function () {
+  const menu = new ContextMenu("#menu");
+  document.body.appendChild(menu.el);
+
+  document.body.addEventListener("contextmenu", (event) => {
+    event.preventDefault();
+    menu.open(event.pageX, event.pageY);
+  });
+});
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,17 @@
-import {Menu} from './core/menu'
+import { Menu } from "./core/menu";
 
 export class ContextMenu extends Menu {
-
+  constructor() {
+    super("#menu");
+    this.el.textContent = "vdsvdsvds";
+  }
+  open(pageX, pageY) {
+    this.el.style = `left: ${pageX}px; top: ${pageY}px`;
+    this.el.classList.add("open");
+    console.log("open");
+  }
+  close() {
+    this.el.classList.remove("open");
+    console.log("close");
+  }
 }
