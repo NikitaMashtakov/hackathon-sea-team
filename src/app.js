ffb3081421f7bb8f5f0f3377f2c3f06be9b8654e
Oleg Portnov <drose1@list.ru>
2025-02-08 16:50:27 +0400

    upd clicks.module.js & add timer.module.js & upd styles.css

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
-import { ContextMenu } from "./menu";
-import { ClearModule } from "./modules/clear.module";
-import { ClicksModule } from "./modules/clicks.module";
-import "./styles.css";
+import { ContextMenu } from './menu';
+import { ClearModule } from './modules/clear.module';
+import { ClicksModule } from './modules/clicks.module';
+import { TimerModule } from './modules/timer.module';
+import './styles.css';
 
-document.addEventListener("DOMContentLoaded", function () {
-  const menu = new ContextMenu("#menu");
-  document.body.appendChild(menu.el);
-  menu.add(new ClicksModule("clicker", "Clicker")); //ДЛЯ ТЕСТА
-  menu.add(new ClearModule("clear", "Clear")); //Обязательный модуль, должен быть последним
+document.addEventListener('DOMContentLoaded', function () {
+	const menu = new ContextMenu('#menu');
+	document.body.appendChild(menu.el);
+	menu.add(new TimerModule());
+	menu.add(new ClicksModule());
+	menu.add(new ClearModule('clear', 'Clear')); //Обязательный модуль, должен быть последним
 });
--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,12 +1,29 @@
-import { Module } from "../core/module";
+import { Module } from '../core/module';
 
 export class ClicksModule extends Module {
-  trigger() {
-    //КОД НИЖЕ ДЛЯ ТЕСТА, МОЖНО УДАЛИТЬ
-    console.log("trigger");
-    const container = document.createElement("div");
-    container.className = "container";
-    container.textContent = "TRIGGER";
-    document.body.append(container);
-  }
+	constructor() {
+		super('clicks', 'Анализ кликов');
+		this.clickCount = 0;
+		this.timeLimit = 3000;
+	}
+
+	trigger() {
+		this.clickCount = 0;
+		alert(`Анализ кликов запущен на ${this.timeLimit / 1000} секунд`);
+
+		const clickHandler = () => this.clickCount++;
+
+		document.addEventListener('click', clickHandler);
+		document.addEventListener('dblclick', clickHandler);
+
+		setTimeout(() => {
+			document.removeEventListener('click', clickHandler);
+			document.removeEventListener('dblclick', clickHandler);
+			alert(
+				`Вы сделали ${this.clickCount} кликов за ${
+					this.timeLimit / 1000
+				} секунд`
+			);
+		}, this.timeLimit);
+	}
 }
