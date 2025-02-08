5b8a3a5b535773720f89c3a3183481d070d10bc3
NikitaMashtakov <99260423+NikitaMashtakov@users.noreply.github.com>
2025-02-08 21:27:08 +0700

    Feature/menu (#3)
    * add menu opener
    
    * add menu, add clear module
    
    * add clearAll util

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,12 @@
-import {Module} from '../core/module'
+import { Module } from "../core/module";
 
 export class ClicksModule extends Module {
-
+  trigger() {
+    //КОД НИЖЕ ДЛЯ ТЕСТА, МОЖНО УДАЛИТЬ
+    console.log("trigger");
+    const container = document.createElement("div");
+    container.className = "container";
+    container.textContent = "TRIGGER";
+    document.body.append(container);
+  }
 }
