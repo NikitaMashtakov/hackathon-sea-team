1169d8f5c030c276e0623223a1ea869ba82c1df2
NikitaMashtakov <99260423+NikitaMashtakov@users.noreply.github.com>
2025-02-08 22:01:03 +0700

    Feature/star sky module (#7)
    * Update and rename background.module.js to starSky.module
    
    * Update app.js
    
    * Update clicks.module.js
    
    * Update menu.js
    
    ---------
    
    Co-authored-by: Darina <darina.dydykina21@gmail.com>

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import { ContextMenu } from './menu';
 import { ClearModule } from './modules/clear.module';
 import { ClicksModule } from './modules/clicks.module';
 import { TimerModule } from './modules/timer.module';
+import { StarSkyModule } from './modules/starSky.module';
 import './styles.css';
 
 document.addEventListener('DOMContentLoaded', function () {
 	const menu = new ContextMenu('#menu');
 	document.body.appendChild(menu.el);
+  menu.add(new StarSkyModule('starSky', 'Звездное небо'));
 	menu.add(new TimerModule('timer', 'Таймер отсчета'));
 	menu.add(new ClicksModule('clicks', 'Анализ кликов'));
 	menu.add(new ClearModule('clear', 'Clear')); //Обязательный модуль, должен быть последним
