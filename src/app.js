046e08dd26e8e6d4482f72cfdc9e2ee95308fd7f
NikitaMashtakov <99260423+NikitaMashtakov@users.noreply.github.com>
2025-02-09 23:42:39 +0700

    Feature/menu (#19)
    * updated clearAll func
    
    * updated clearAll func one more time

--- src/app.js
+++ src/app.js
@@ -8,16 +8,18 @@ import { SoundsModule } from "./modules/sounds.module";
 import { ShapeModule } from "./modules/shape.module";
 import { RandomMessage } from "./modules/RandomMessage.module";
 import { CityCardsModule } from "./modules/cityCards.module";
+import { greetingMessage } from "./utils";
 
 document.addEventListener("DOMContentLoaded", () => {
+  greetingMessage();
   const menu = new ContextMenu("#menu");
   document.body.appendChild(menu.el);
   menu.add(new StarSkyModule("starSky", "Звездное небо"));
   menu.add(new TimerModule("timer", "Таймер отсчета"));
-  menu.add(new ClicksModule("clicks", "Анализ кликов", 5000));
-  menu.add(new SoundsModule("sounds", "случайный звук"));
-  menu.add(new RandomMessage("message", "ваше сообщения"));
-  menu.add(new ShapeModule("div", "контейнер"));
-  menu.add(new CityCardsModule("cards", "карточки"));
-  menu.add(new ClearModule("clear", "Clear")); //Обязательный модуль, должен быть последним
+  menu.add(new ClicksModule("clicks", "Кликометр", 5000));
+  menu.add(new SoundsModule("sounds", "Случайный звук"));
+  menu.add(new RandomMessage("message", "Рандомное сообщение"));
+  menu.add(new ShapeModule("div", "Рандомная фигура"));
+  menu.add(new CityCardsModule("cards", "Фотогалерея"));
+  menu.add(new ClearModule("clear", "Очистить всё")); //Обязательный модуль, должен быть последним
 });
